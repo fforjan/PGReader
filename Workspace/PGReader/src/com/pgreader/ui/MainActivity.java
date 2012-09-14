907da50c91376c64d01276a481eddd9c3bf72477
Frederic Forjan <fforjan@free.fr>
2012-09-14 20:24:22 +1000

    Introduce the main activity for displaying the main menu

---
+++ Workspace/PGReader/src/com/pgreader/ui/MainActivity.java
@@ -0,0 +1,40 @@
+package com.pgreader.ui;
+
+import com.pgreader.R;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+
+
+/**
+ * our main activity.
+ * @author GeoVah 
+ */
+public class MainActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        
+        final Button button = (Button) findViewById(R.id.unitBrowser);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+            	// start the activity for display the unit browser
+            	Intent unitListActivity = new Intent(v.getContext(), UnitListActivity.class);
+                startActivity(unitListActivity);
+            }
+        });
+    }
+    
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_main, menu);
+        return true;
+    }
+}
