cc228c133686aba61a695d2bec6a9be6c798de5b
Frederic Forjan <fforjan@free.fr>
2012-09-15 23:18:04 +1000

    remove unused import

--- Workspace/PGReader/src/com/pgreader/data/DataRepository.java
+++ Workspace/PGReader/src/com/pgreader/data/DataRepository.java
@@ -5,9 +5,6 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import android.content.Context;
-import android.media.MediaPlayer;
-
 /**
  * data repository for reading legacy PG data.
  */
